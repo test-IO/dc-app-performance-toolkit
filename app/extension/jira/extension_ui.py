6ffc233233183ae9c730a24b315c02ce7122dab7
Norbert Patai <Norbert_Patai@epam.com>
2021-10-06 09:30:08 +0200

    extend timeout to 60

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -14,19 +14,7 @@ def app_accept_testio_bug(webdriver):
     def measure():
         @print_timing("selenium_app_accept_testio_bug:accept_testio_bug")
         def sub_measure():
-            page.wait_until_visible((By.ID, "test-io-accept-button")).click()  # Accept testio bug
-        sub_measure()
-    measure()
-
-def app_reject_testio_bug(webdriver):
-    page = BasePage(webdriver)
-    project_key = 'UOO'
-    page.go_to_url(f"{JIRA_SETTINGS.server_url}/projects/{project_key}/test-io-issues")
-    @print_timing("selenium_reject_testio_bug")
-    def measure():
-        @print_timing("selenium_reject_testio_bug:reject_testio_bug")
-        def sub_measure():
-            page.wait_until_visible((By.ID, "test-io-reject-button")).click()  # Reject testio bug
+            page.wait_until_visible((By.ID, "test-io-accept-button"), 60).click()  # Accept testio bug
         sub_measure()
     measure()
 
@@ -38,10 +26,10 @@ def app_change_severity_testio_bug(webdriver):
     def measure():
         @print_timing("selenium_app_change_severity_testio_bug:change_testio_bug_severity")
         def sub_measure():
-            page.wait_until_visible((By.ID, "test-io-change-severity-button")).click()  # Change testio bug severity
-            page.wait_until_visible((By.ID, "new-severity-selector-input")).click()
-            page.wait_until_visible((By.CLASS_NAME, "aui-select-suggestion")).click()
-            page.wait_until_visible((By.ID, "change_issue_severity_submit")).click()
+            page.wait_until_visible((By.ID, "test-io-change-severity-button"), 60).click()  # Change testio bug severity
+            page.wait_until_visible((By.ID, "new-severity-selector-input"), 60).click()
+            page.wait_until_visible((By.CLASS_NAME, "aui-select-suggestion"), 60).click()
+            page.wait_until_visible((By.ID, "change_issue_severity_submit"), 60).click()
         sub_measure()
     measure()
 
@@ -52,10 +40,10 @@ def view_testio_specific_bug(webdriver):
     @print_timing("selenium_app_custom_action:view_testio_bugs")
     def sub_measure():
         page.go_to_url(f"{JIRA_SETTINGS.server_url}/projects/{project_key}/test-io-issues")
-        page.wait_until_visible((By.CLASS_NAME, "test-io-issue"))  # Wait for you test-io-issues-list UI element by ID selector
+        page.wait_until_visible((By.CLASS_NAME, "test-io-issue"), 60)  # Wait for you test-io-issues-list UI element by ID selector
     sub_measure()
 
     @print_timing("selenium_app_custom_action:open_specific_testio_bug")
     def sub_measure():
-        page.wait_until_visible((By.CLASS_NAME, "test-io-issue")).click()  # Open specific testio bug
+        page.wait_until_visible((By.CLASS_NAME, "test-io-issue"), 60).click()  # Open specific testio bug
     sub_measure()
